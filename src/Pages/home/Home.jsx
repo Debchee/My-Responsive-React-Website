d574d4ac4455517b929c9ac080457d7d0a70aebb
Debchee <debbinwonye456@gmail.com>
2022-08-27 21:01:30 -0700

    added programs section

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import './Home.css';
 import MainHeader from '../../Components/MainHeader';
+import Programs from '../../Components/Programs';
 
 const Home = () => {
   return (
     <>
       <MainHeader/>
+      <Programs/>
     </>
     )
 }
